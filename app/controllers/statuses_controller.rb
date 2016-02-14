a86b90d57c9fa41fd48ae2fa8bd811b6d749ee11
bizygrove <bizygrove@hotmail.co.uk>
2016-02-14 18:49:03 +0000

    building better social network

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,5 +1,5 @@
 class StatusesController < ApplicationController
-  before_action :set_status, only: [:show, :edit, :update, :destroy]
+  before_action  :authenticate_user!, :set_status, only: [:show, :edit, :update, :destroy]
 
   # GET /statuses
   # GET /statuses.json
@@ -14,7 +14,7 @@ class StatusesController < ApplicationController
 
   # GET /statuses/new
   def new
-    @status = Status.new
+    @status = current_user.statuses.new
   end
 
   # GET /statuses/1/edit
@@ -24,10 +24,11 @@ class StatusesController < ApplicationController
   # POST /statuses
   # POST /statuses.json
   def create
-    @status = Status.new(status_params)
+    @status = current_user.statuses.new(status_params)
 
     respond_to do |format|
-      if @status.save
+      if @status.user == current_user
+        @status.save
         format.html { redirect_to @status, notice: 'Status was successfully created.' }
         format.json { render :show, status: :created, location: @status }
       else
@@ -41,7 +42,8 @@ class StatusesController < ApplicationController
   # PATCH/PUT /statuses/1.json
   def update
     respond_to do |format|
-      if @status.update(status_params)
+      if @status.user == current_user
+        @status.update(status_params)
         format.html { redirect_to @status, notice: 'Status was successfully updated.' }
         format.json { render :show, status: :ok, location: @status }
       else
@@ -54,10 +56,14 @@ class StatusesController < ApplicationController
   # DELETE /statuses/1
   # DELETE /statuses/1.json
   def destroy
-    @status.destroy
-    respond_to do |format|
+    if @status.user == current_user
+      @status.destroy
+      respond_to do |format|
       format.html { redirect_to statuses_url, notice: 'Status was successfully destroyed.' }
       format.json { head :no_content }
+    end
+    else
+      redirect_to statuses_path, notice: 'You cant delete this item'
     end
   end
 
@@ -69,6 +75,6 @@ class StatusesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def status_params
-      params.require(:status).permit(:name, :content)
+      params.require(:status).permit(:content, :user_id)
     end
 end
