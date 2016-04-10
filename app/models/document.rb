18e2967370eaf35d546c0216e814f324607b8114
bizygrove <bizygrove@hotmail.co.uk>
2016-04-10 21:50:09 +0100

    allowing image to be stored somewhere

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,4 +1,7 @@
 class Document < ActiveRecord::Base
-	has_attached_file :add_attachment, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/default.png"
+	has_attached_file :add_attachment,
+					styles: { medium: "300x300>", thumb: "100x100>" },
+					:url => "/assets/image/:class/:basename.:extension",
+					:path => ":rails_root/public/assets/image/:class/:basename.:extension"
   	validates_attachment_content_type :add_attachment, content_type: /\Aimage\/.*\Z/
 end
