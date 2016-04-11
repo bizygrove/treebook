a30fb35ec7f0f8b4dc269956b28a6d9fdd4490b2
bizygrove <bizygrove@hotmail.co.uk>
2016-04-11 20:45:10 +0100

    reverting my chnges to document.rb

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,7 +1,5 @@
 class Document < ActiveRecord::Base
 	has_attached_file :add_attachment,
-					styles: { medium: "300x300>", thumb: "100x100>" },
-					:url => "/assets/image/:class/:basename.:extension",
-					:path => ":rails_root/public/assets/image/:class/:basename.:extension"
+					styles: { medium: "300x300>", thumb: "100x100>" }
   	validates_attachment_content_type :add_attachment, content_type: /\Aimage\/.*\Z/
 end
